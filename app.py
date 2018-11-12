d4101676357946e4372166138478c0367f79a7d3
Giovanni Pecchio <gp30@rice.edu>
2018-11-11 22:53:55 -0600

    Added form that sends a post request and then the backend handles it by running a search and returing the results as an array of 'posts'

--- app.py
+++ app.py
@@ -1,37 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from search import Search
 
 app = Flask(__name__)
 
 
-@app.route('/hello')
-def hello_world():
-    return 'Hello World!'
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-@app.route('/search/<terms>/<sort>/<tag>/<pg_number>')
-def search(terms, sort, tag, pg_number):
+@app.route('/search', methods=['POST', 'GET'])
+def search():
     """
     All of the parameters will be strings and then the backend will handle them
     """
-    new_search = Search()
-    # Set what the terms for the search are
-    new_search.set_query(terms)
+    if request.method == 'POST':
+        # Assign each search parameter to each given argument
+        terms = request.form['terms']
+        sort = request.form['sort']
+        tags = request.form['tags']
+        pg_number = request.form['pg_number']
 
-    # Set whether to sort the results or not
-    new_search.set_sort_by_date(sort)
+        # instantiate a new search
+        new_search = Search()
 
-    # Set what tags will the content be filtered by
-    new_search.set_tags(tag)
+        # Set the arguments for the search
+        new_search.set_query(terms)
+        new_search.set_sort_by_date(sort)
+        new_search.set_tags(tags)
+        new_search.set_page_number(pg_number)
 
-    # Ser the page number for the search
-    new_search.set_page_number(pg_number)
+        # Run the query with the given parameters
+        posts = new_search.run_query()
 
-    # Run the query with the given parameters
-    posts = new_search.run_query()
-
-    return 'posts %s' % posts
-    #return render_template('search_results.html', posts=posts)
+        return render_template('search_results.html', posts=posts)
+    else:
+        return redirect(url_for('/'))
 
 
 if __name__ == '__main__':
